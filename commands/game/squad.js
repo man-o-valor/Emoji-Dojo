8a68a124a1593340ce610c6c5f76f47443f8101b
Man-o-Valor <valoregjertsen@gmail.com>
2025-01-24 23:11:26 -0500

    it did not work

--- commands/game/squad.js
+++ commands/game/squad.js
@@ -55,7 +55,8 @@ module.exports = {
 						const row2 = new ActionRowBuilder()
 							.addComponents(ready);
 						const readyresponse = await interaction2.reply({content:`Here is a list of all the Emojis you have in your Dojo. Type out a message of 8 Emojis for your Squad and copy it.\nThis message will become your new Squad, from back to front (order matters!). Once you've copied it, click the button below.\n(Note: To read their abilities and stats, run \`/dojo\` with the name of the emoji.)\n\n${vaulttext}`,flags: 'Ephemeral',components:[row2]})
-						let collector = readyresponse.createMessageComponentCollector({ time: 120000 });
+						const collectorFilter = i => i.user.id == interaction.user.id
+						let collector = readyresponse.createMessageComponentCollector({ filter: collectorFilter, time: 120000 });
 							try {
 								collector.on('collect', async (interaction3) => {
 									console.log("Got here")
