20227c6e871046a65c32d478b46c0cdaa59691c0
Man-o-Valor <valoregjertsen@gmail.com>
2025-02-23 13:35:40 -0500

    Starting log testing! Expecting failure

--- commands/game/battlebot.js
+++ commands/game/battlebot.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder,ButtonBuilder,ButtonStyle,ActionRowBuilder } = require('discord.js');
 const {emojis} = require('../../data.js')
-const {getsquad,allemojisofrarity,playturn,database,coinschange,trysetupuser} = require('../../functions.js')
+const {getsquad,allemojisofrarity,playturn,database,coinschange,trysetupuser,getlogs,writelogs} = require('../../functions.js')
 const lodash = require('lodash');
 
 module.exports = {
@@ -20,7 +20,13 @@ module.exports = {
 			}
 			const bp = await database.get(interaction.user.id + "battlepending") ?? "0"
 			const bbcd = "0"//await database.get(interaction.user.id + "botbattlecooldown") ?? "0"
+			// UNCOMMENT THIS WHEN RELEASING
 			if (bp < Date.now()/1000 && bbcd < Date.now()/1000) {
+				let logs = getlogs()
+				(logs.logs.games.botopened ?? 0) += 1
+				(logs.logs.games.opened ?? 0) += 1
+				(logs.logs.players[`user${interaction.user.id}`] = logs.logs.players[`user${interaction.user.id}`] ?? {})
+				(logs.logs.players[`user${interaction.user.id}`].opened ?? 0) += 1
 				const cook = new ButtonBuilder()
 					.setCustomId('battle')
 					.setLabel('Battle!')
